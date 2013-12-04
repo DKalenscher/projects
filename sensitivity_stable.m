e5437a17395cfd97520b3466eece17394a87c545
DKalenscher <DKalenscher@gmail.com>
2013-12-04 14:49:07 -0500

    Update and rename dan.m to sensitivity_stable.m
    Program now dynamically adds variables for number of constraints based on A and b.

--- sensitivity_stable.m
+++ sensitivity_stable.m
@@ -1,5 +1,14 @@
-%CURRENT ISSUES:  IF 2 VALUES ON Z ARE THE SAME I GET AN ERROR
-%                   VALUES AREN'T UPDATING INSIDE THE LOOP
+%Daniel Kalenscher
+%Last Updated- 12/4/2013
+% NEED TO TAKE INTO ACCOUNT FREE VARIABLES
+
+% ONLY RUNS PHASE II, NEED TO ADD PHASE I CONVERSION
+
+% add option to determine minimum or maximum
+%Should add in error checking loops on input values
+
+
+%SHADOW PROFIT = profit(b1 +1) - profit.  Its the difference between the profit of the altered vs how it would have been originally for each. Can be negaive or positive)                   
 
 %Assign values to A,b,P and build the Table to be prepared for ljx
 prompt = 'Please enter initial values of A in array format: ';
@@ -89,24 +98,38 @@ elseif selection == 'P';
 
     
 else
-       
+    disp('Running Single Min/Max Algorithm');
+    inc=1;
+    increment=0;
 end
 
+%===================Only changes the matrics of A,b,P for the selected
+%value.=====================
+    if selection== 'A';
+        A(rowpos,colpos)=minval;
 
+    elseif selection == 'b';
+        b(rowpos,colpos)=minval;
 
+    elseif selection == 'P';
 
+        P(rowpos,colpos)=minval;
 
+    else
+        
+    end
 
 
 
 %startvar = minval;
 %for i=startvar:increment:maxval  COULD PROBABLY JUST USE INC DIRECTLY
-    
+
+count=1;  %  counters for populating end result arrays, needs to initialize outside of For loop
+changeval=1;
+
 for i=1:1:inc   
+
     
-A(rowpos,colpos)=minval;
-b(rowpos,colpos)=minval;
-P(rowpos,colpos)=minval;
 
 
 %Need a for loop. For(locationvar=minval; location variable
@@ -120,7 +143,7 @@ P(rowpos,colpos)=minval;
 
 
 
-T=totbl(A,b,P);
+T=totbl(A,b,P)
 
 
 
@@ -151,26 +174,38 @@ end
 
 
 [r,c]=find(z(1:colnum-1)==min(min(z(1:colnum-1))));  % finds the row(r) and column(c) of the minimum value for the array of z we created
+r=r(1,1);   % We reset the values of R and C to only take the first value of the minimum locations incase there are 2 or more equal numbers
+c=c(1,1);
 
-
-
+        
+        
 %Next I need to find the location of the row that has the lowest ratio for
 %this column
 
 %c is the value of the column with the lowest number, need to take the full
 %value of that column
 
-Lowcol=T.val(:,c);   % puts the values of column c into lowcol
+Lowcol=T.val(:,c);  % puts the values of column c into lowcol
 
 colnum=size(T.val,2); % counts the number of columns in the T matrix and assigns that to colnum
 rownum=size(T.val,1); % counts the number of columns in the T matrix and assigns that to colnum
 
 Lastcol=T.val(:,colnum); % puts the values of the last column into Lastcol
 
-ratio=Lastcol./Lowcol; % divides the last col by the lowest col  WE NEED TO GET THE LARGEST NEGATIVE NUMBER NOW
+ratio=Lastcol./Lowcol % divides the last col by the lowest col  WE NEED TO GET THE LARGEST NEGATIVE NUMBER NOW
 
-[C,I]=max(ratio(ratio<0));%WE NEED TO GET THE LARGEST NEGATIVE NUMBER NOW
-% max(ratio(ratio(1:rownum-1) < 0))  WOULD BE A BETTER SOLUTION TO THE
+
+
+%[C,I]=max(ratio(ratio<0)) %WE NEED TO GET THE LARGEST NEGATIVE NUMBER NOW
+
+ratio(~isfinite(ratio))=0  % replaces Inf values with 0 so they don't get counted in.
+ratio_max = max(ratio(ratio(1:rownum-1) < 0));
+
+I = find(ratio==ratio_max);
+I = I(1,1);  % Sets I equal to the first value of I in case there are 2 or more equal ratio values.
+
+%[C,I]= max(ratio(ratio(1:rownum-1) < 0))  WOULD BE A BETTER SOLUTION TO THE
+
 % ABOVE NEED TO TEST SYNTAX LATER
 
 %%[C,I]=min(ratio(1:rownum-1));% finds the minimum value of the ratio between positions 1 and the second to last value and assigns the positon to I
@@ -195,13 +230,86 @@ end
 end
 
 disp('MINIMIZATION ITERATION IS COMPLETE');
-A
-b
-P
+
 %NEED TO ALLOW USERS TO ENTER A RANGE OF VALUES FOR A,b,P AND RUN EACH
 %ITERATION.
 
 % NEED TO SET VALUES FOR X1, X2, Z for each loop run so we can plot a
 % table.
-end  % END FOR LOOP
 
+
+%===================Only INCREMENTS the matrics of A,b,P for the selected
+%value.=====================
+    if selection== 'A';
+        A(rowpos,colpos)=(minval+increment); 
+        minval=(minval+increment);
+   
+    elseif selection == 'b';
+        b(rowpos,colpos)=(minval+increment);
+        minval=(minval+increment);
+   
+    elseif selection == 'P';
+        P(rowpos,colpos)=(minval+increment);
+        minval=(minval+increment);
+   
+    end
+A
+b
+P
+%==========Dynamically creates/populates X(n) values as well as
+%Z=============%
+
+
+
+Zvalues(count) = abs(T.val(rownum,colnum));
+
+
+for i=1:1:colnum-1     %Loop to determine number of X values needed
+
+    if ( all(z(1:colnum-1)) ); %If all Z values are nonzero (and therefore > 0)
+        x_var_row=find(ismember(T.bas,['x', num2str(i)])); % see if X(n) value is on side or top of table
+          
+        if (isempty(x_var_row));   %if not on the side, it is on top and therefore 0 since Z values are > 0.
+            eval(['x' num2str(i) '(' num2str(count) ') = ' num2str(0)]);
+        else
+            eval(['x' num2str(i) '(' num2str(count) ') = ' num2str((T.val(x_var_row,colnum)))]);  % otherwise put the number in the last col of the table directly into X(n)
+        end
+    end
+end
+
+
+
+
+
+%========================Original Statically created X(n) variables======
+%if ( all(z(1:colnum-1)) );  % if all values in Z are above zero, there are no free variables so values for x1/x2 can be set directly
+ %   x1row=find(ismember(T.bas,'x1'));
+        
+  %  if (isempty(x1row));  %checks to see if variable is on left side or top of matrix if on top, then x1 is 0 because all Zs are positive
+ %       x1(count) = 0;  % if x1 is on top then it is 0 because we have already checked that the Z values are not 0
+   % else
+    %x1(count) = (T.val(x1row,colnum)); 
+   % end
+
+ %   x2row=find(ismember(T.bas,'x2'))
+    
+  %  if (isempty(x2row));  %checks to see if variable is on left side or top of matrix if on top, then x2 is 0 because all Zs are positive
+   %     x2(count) = 0;  % if x1 is on top then it is 0 because we have already checked that the Z values are not 0
+   % else
+  %  x2(count) = (T.val(x2row,colnum));
+   % end
+
+%end
+
+change_val(count) = minval;
+
+
+count = count+1;
+
+end  % END FOR LOOP
+Zvalues
+x1
+x2
+
+% Only necessary if there are 2 X(n) values
+plot3(x1,x2,change_val)
